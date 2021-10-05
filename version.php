cdf61c4d9af565848d3a3d99f80de1fa35bd9b35
Simon Schniedenharn <simon.schniedenharn@its.thm.de>
2021-10-05 11:15:16 +0200

    Update: table layout for signed quiz resultsd Version: Bump

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017011303;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017011304;       // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_signed_quiz_export'; // Full name of the plugin (used for diagnostics)
